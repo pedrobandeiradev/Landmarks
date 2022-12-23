0d9fb41c759aa463a1c28534287ee4273e9478a2
Pedro Bandeira <pedrobandeira.dev@gmail.com>
2022-12-23 14:16:47 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Pedro Bandeira on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
